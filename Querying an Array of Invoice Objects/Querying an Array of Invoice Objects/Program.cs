4e42a41e60efefc7116bc9437a0b73d9f9e20f8d
Ensar Yiğit Sarıtaş <en_sar78500@outlook.com>
2024-01-21 22:46:15 +0300

    first commit

---
+++ Querying an Array of Invoice Objects/Querying an Array of Invoice Objects/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Querying_an_Array_of_Invoice_Objects
+{
+	//Fatura classı
+	class Invoice
+	{
+		public int PartNumber { get; set; }
+		public string PartDescription { get; set; }
+		public int Quantity { get; set; }
+		public double Price  { get; set; }
+
+		//4 parametreli Invoice constructerı
+		public Invoice(int partNumber, string partDescription, int quantity, double price)
+		{
+			this.PartNumber = partNumber;
+			this.PartDescription = partDescription;
+			this.Quantity = quantity;
+			this.Price = price;
+		} 
+
+	}
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//Tablo bilgileri nesneler halinde dizi elemanları oluyor.
+			Invoice[] invoice = new Invoice[]
+			{
+				new Invoice  (83, "Electric sander", 7, 57.98 ),
+				new Invoice  (24, "Power saw", 18, 99.99 ),
+				new Invoice  (7, "Sledge hammer", 11, 21.50),
+				new Invoice  (77, "Hammer", 76, 11.99),
+				new Invoice  (39, "Lawn mower", 3, 79.50),
+				new Invoice  (68, "Screwdriver", 106, 6.99),
+				new Invoice  (56, "Jig saw", 21, 11.00),
+				new Invoice  (3, "Wrench", 34, 7.50)
+			};
+
+			//A) Sorted the Invoice objects by PartDescription 
+			var FilteredPartDescription = from items in invoice
+								  orderby items.PartDescription
+								  select items.PartDescription;
+			
+			//Foreach ile LINQ ile filtrelenenler yazdırılıyor.
+			foreach (var item in FilteredPartDescription)
+			{
+				Console.WriteLine($"Part Description : {item}");
+			}
+
+            Console.WriteLine("\n*************************************************\n");
+
+            //B) Sorted the Invoice objects by Price
+            var FilteredPrice = from items in invoice
+								orderby items.Price
+								select items;
+            
+			//Foreach ile LINQ ile filtrelenenler yazdırılıyor.
+            foreach (var item in FilteredPrice)
+			{
+				Console.WriteLine($"Price : {item.Price}");
+			}
+
+            Console.WriteLine("\n*************************************************\n");
+
+            //C) Selected the PartDescription and Quantity and sorted the results by Quantity
+            var FilteredQuantity = from items in invoice
+								   orderby items.Quantity
+								   select new { items.PartDescription, items.Quantity };
+            
+			//Foreach ile LINQ ile filtrelenenler yazdırılıyor.					   
+            foreach (var item in FilteredQuantity)
+            {
+				Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\n*************************************************\n");
+
+            //D) Selected PartDescription, InvoiceTotal and sorted the results by InvoiceTotal
+            var flterdTotal = from items in invoice
+						let InvoiceTotal = items.Quantity * items.Price
+						orderby InvoiceTotal
+						select new { items.PartDescription, InvoiceTotal };
+            
+			//Foreach ile LINQ ile filtrelenenler yazdırılıyor.
+            foreach (var item in flterdTotal)
+			{
+				Console.WriteLine(item);
+			}
+
+            Console.WriteLine("\n*************************************************\n");
+
+            //E) Using the results of the LINQ query in part (d), select the InvoiceTotals in the range $200 to $500.
+            var condition200to500 = from items in flterdTotal
+									where 200 < items.InvoiceTotal && items.InvoiceTotal < 500
+									select items;
+            
+			//Foreach ile LINQ ile filtrelenenler yazdırılıyor.
+            foreach (var item in condition200to500)
+			{
+				Console.WriteLine(item);
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ Sorting Pets Names by Length/Sorting Pets Names by Length/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Sorting_Pets_Names_by_Length
+{
+	internal class Program
+	{
+        //AddInputPetNames fonksiyonu döngü ifadesi break komutunu kullanana kadar devam eder. Kullanıcının listeye ekleme işlemi bittiğinde 'Quit' yazması gerekir. Kullanıcı Pet ismini listeye ekler.
+        //Eğer eklenen Pet ismi Alfabetik ve boşluktan oluşmuyorsa (kontrol IsValidName fonksiyonu ile yapılır) hata mesajı bastırılır.
+        static void AddInputPetNames(List<string> petNames)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the pet name(or 'quit' to exit) : ");
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "quit")
+                    break;
+
+                if (IsValidName(input))
+                    petNames.Add(input);
+                else
+                    Console.WriteLine("Invalid pet name! Pet name should not contain numbers or special characters.");
+            }
+        }
+        //IsValidName fonksiyonu Pet ismi Alfabetik ve boşluktan oluşma durumunu kontrol eder. Regex metin tabanlı verileri aramak için özel bir ifade ismidir.
+        //Girilen metin ifadesi a-z, A-Z ve \s (space karakteri)'den aykırı bir karaktere sahipse return değeri false döner.
+        static bool IsValidName(string name)
+        {
+            return (!Regex.IsMatch(name, @"[^a-zA-Z\s]"));
+        }
+
+        static void Main(string[] args)
+		{
+            //petNames isimli liste tanımı
+			List<string> petNames = new List<string>();
+
+			AddInputPetNames(petNames);
+
+            //A) Sorted the List in ascending order on the basis of number of characters in the name.
+            var sortedNameSize = from count in petNames
+								 orderby count.Length
+								 select count;
+			
+            Console.WriteLine("\n******************************************");
+            Console.WriteLine("Pet names listed in ascending order : \n");
+            
+            foreach (var name in sortedNameSize)
+			{
+				Console.WriteLine(name);
+			}
+
+            //B) Sorted the List in ascending order on the basis of number of characters in the name.
+            var sortedDescendingNameSize = from count in petNames
+                                           orderby count.Length descending
+                                           select count;
+            
+            Console.WriteLine("\n******************************************");
+            Console.WriteLine("Pet Names listed in descending order : \n");
+            
+            foreach (var name in sortedDescendingNameSize)
+            {
+                Console.WriteLine(name);
+            }
+
+            //C) Pet Names listed in ascending order after removing duplicate names
+            var sortedDuplicateNameSize = from count in petNames
+                                          orderby count.Length
+                                          select count;
+            
+            Console.WriteLine("\n******************************************");
+            Console.WriteLine("Pet Names listed in ascending order after removing duplicate names : \n");
+            
+            foreach (var name in sortedDuplicateNameSize.Distinct())
+            { 
+                Console.WriteLine(name);
+            }
+
+            Console.ReadLine();
+		}
+	}
+}
